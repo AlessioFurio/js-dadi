bf2d84dc09c6d2a6248984e57c941c7f8a38e5f8
Furio Alessio <alefurio@hotmail.it>
2020-10-14 22:05:54 +0200

    gioco dadi aggiunta ciclo for di 10 partite

--- scripts.js
+++ scripts.js
@@ -3,18 +3,31 @@
 // Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 
-//genero numero random per giocatore 1
-var giocatore1 = Math.floor(Math.random() * 6) + 1;
 
-// genero numero random per giocatore 2
-var giocatore2 = Math.floor(Math.random() * 6) + 1;
 
-console.log(giocatore1);
-console.log(giocatore2);
+var contatore1 = 0;
+var contatore2 = 0;
+var numeroPartite = 10;
+
+var vincitore1 = true;
+var vincitore2 = false;
+
+
+for (var i = 0; i < numeroPartite; i++) {
+    //genero numero random per giocatore 1
+    var giocatore1 = Math.floor(Math.random() * 6) + 1;
+
+    // genero numero random per giocatore 2
+    var giocatore2 = Math.floor(Math.random() * 6) + 1;
+
+    console.log(giocatore1);
+    console.log(giocatore2);
 
 //vittoria giocatore1
 if (giocatore1 > giocatore2) {
     console.log('giocatore 1 ha vinto');
+    vincitore1 = true;
+    contatore1++;
 }
 //pareggio
 else if (giocatore1 == giocatore2) {
@@ -23,15 +36,12 @@ else if (giocatore1 == giocatore2) {
 //vittoria giocatore2
 else {
     console.log('giocatore 2 ha vinto');
+    vincitore2 = false;
+    contatore2++;
+}
 }
 
-
-//conto quante volte vince un giocatore
-var contatore1 = 0;
-var contatore2 = 0;
+    console.log('il 1 ha vinto ' + contatore1 + 'volte');
 
 
-
-for (var i = 0; i < array.length; i++) {
-    array[i]
-}
+    console.log('il 2 ha vinto ' + contatore2 + 'volte');
