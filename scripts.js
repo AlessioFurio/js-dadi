1ef4a53fb56e77f65a29aeb33e854a6780008494
Furio Alessio <alefurio@hotmail.it>
2020-10-15 00:08:19 +0200

    aggiunta calcolo vittorie su 10 match e log su chi ne ha vinti di più

--- scripts.js
+++ scripts.js
@@ -5,29 +5,84 @@
 
 var arrayGiocatori = [1, 2];
 var risultato;
+var numMatch = 10;
+var contatoreGio1 = 0;
+var contatoreGio2 = 0;
 
-for (var i = 0; i < arrayGiocatori.length; i++) {
 
-    arrayGiocatori[i] = Math.floor(Math.random() * 6) + 1;
-    console.log(arrayGiocatori[i]);
 
-    if (arrayGiocatori[0] < arrayGiocatori[1]) {
-        risultato = false;
+
+
+for (var j = 0; j < numMatch; j++) {
+
+    for (var i = 0; i < arrayGiocatori.length; i++) {
+
+        arrayGiocatori[i] = Math.floor(Math.random() * 6) + 1;
+        console.log(arrayGiocatori[i]);
     }
-    else if (arrayGiocatori[0] > arrayGiocatori[1]) {
-        risultato = true;
+
+        if (arrayGiocatori[0] < arrayGiocatori[1]) {
+            risultato = false;
+            console.log('vince giocatore 2');
+        }
+        else if (arrayGiocatori[0] > arrayGiocatori[1]) {
+            risultato = true;
+            console.log('vince giocatore 1');
+        }
+        else if (arrayGiocatori[0] == arrayGiocatori[1]) {
+            risultato = null;
+            console.log('pareggio');
+        }
+
+        if (risultato == false){
+            contatoreGio2++;
+        }
+        else if (risultato == true) {
+            contatoreGio1++;
+        }
     }
-    else if (arrayGiocatori[0] == arrayGiocatori[1]) {
-        risultato = null;
-    }
+
+
+if (contatoreGio1 < contatoreGio2){
+    console.log('giocatore 2 ha vinto più match');
+}
+else if (contatoreGio1 > contatoreGio2) {
+    console.log('giocatore 1 ha vinto più match');
+}
+else if (contatoreGio1 == contatoreGio2) {
+    console.log('giocatore 1 e il giocatore 2 hanno vinto lo stesso numero di match');
 }
 
-if (risultato == false){
-    console.log('giocatore 2 ha vinto');
-}
-else if (risultato == true) {
-    console.log('giocatore 1 ha vinto');
-}
-else if (risultato == null) {
-    console.log('Pareggio');
-}
+
+
+
+
+
+// var arrayGiocatori = [1, 2];
+// var risultato;
+//
+// for (var i = 0; i < arrayGiocatori.length; i++) {
+//
+//     arrayGiocatori[i] = Math.floor(Math.random() * 6) + 1;
+//     console.log(arrayGiocatori[i]);
+//
+//     if (arrayGiocatori[0] < arrayGiocatori[1]) {
+//         risultato = false;
+//     }
+//     else if (arrayGiocatori[0] > arrayGiocatori[1]) {
+//         risultato = true;
+//     }
+//     else if (arrayGiocatori[0] == arrayGiocatori[1]) {
+//         risultato = null;
+//     }
+// }
+//
+// if (risultato == false){
+//     console.log('giocatore 2 ha vinto');
+// }
+// else if (risultato == true) {
+//     console.log('giocatore 1 ha vinto');
+// }
+// else if (risultato == null) {
+//     console.log('Pareggio');
+// }
